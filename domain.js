ce91277fdbf395381404f6c686774e1de283dbc0
Yojith189 <77870739+Yojith189@users.noreply.github.com>
2021-11-11 15:30:10 +0530

    Add files via upload

---
+++ domain.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+function black(){
+    document.getElementById("z").style.backgroundColor="gray"
+    document.getElementsByClassName("89").style.color="white"
+
+}
+
+function blac(){
+    document.getElementById("z").style.backgroundColor="white"
+    document.getElementsByClassName("89").style.color="black"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
